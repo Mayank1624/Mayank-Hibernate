030f1496f32a3e0a57f566c3dd51d8c09735c47d
Mayank Srivastava <mayankshrivastava1624@gmail.com>
2026-07-28 19:16:26 +0530

    update

--- src/main/java/com/hbn/learning/Main.java
+++ src/main/java/com/hbn/learning/Main.java
@@ -11,23 +11,38 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Employee emp = new Employee( "Dilshad", "male", 79000);
-
-		
-		
-		
-		
+		Employee emp = new Employee( "Mukul", "male", 79000);
 	
-		
-		
-		
 		
 		Session session = HibernateConfig.getSessionFactory() .openSession();
 
 		Transaction transaction = session.beginTransaction();
 
-		session.persist(emp);
-		transaction.commit();
+//		session.persist(emp);
+//		transaction.commit();
+		
+		
+//		Employee employee = session.find(Employee.class, 5);
+//		System.out.println(employee);
+		
+		
+		
+//		session.load(emp, 1);
+//		System.out.println(emp);
+		
+		
+		
+//		Employee employee = session.find(Employee.class, 35);
+//	System.out.println(employee);
+		
+		
+//		session.load(emp, 35);
+//		System.out.println(emp);
+		
+		
+		
+		// find aur load me differenceagar hum find se vo data mangenge jo databas me nhi hai jaise 23 id nhi hai agar hum id ki jagah 23 rakhte hai to ye null vapas karega aur load error dega dono me yhi difference hai
+		
 
 	}
 
