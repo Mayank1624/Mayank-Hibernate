4df821c1d70a0d745647fa640157d6c3a62abe55
Mayank Srivastava <mayankshrivastava1624@gmail.com>
2026-07-28 15:26:48 +0530

    update

--- src/main/java/com/hbn/learning/Main.java
+++ src/main/java/com/hbn/learning/Main.java
@@ -11,9 +11,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Employee emp = new Employee( "Ansh", "male", 99000);
+		Employee emp = new Employee( "Dilshad", "male", 79000);
 
-
+		
+		
+		
+		
+	
 		
 		
 		
@@ -28,3 +32,4 @@ public class Main {
 	}
 
 }
+
